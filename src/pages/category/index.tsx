bed5e90cbfc4021c73018617957899712b80b95d
alessandro <alessandrodamata6@gmail.com>
2023-04-23 19:04:55 -0300

    criado a tela de cadastrar categoria e feito a função de adicionar a caterogia no banco de dados

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,56 @@
+import { useState, FormEvent} from "react";
+import { Header } from "../../components/Header";
+import Head from "next/head";
+import styles  from "./styles.module.scss"
+
+import { setupAPIClient } from "../../service/api";
+import { toast } from "react-toastify";
+
+export default function Category(){
+
+    const [name, setName] = useState("");
+
+    async function  handleRegister(event: FormEvent) {
+        event.preventDefault();
+        if( name === ''){
+            return;
+        }
+
+        const apiClient = setupAPIClient();
+        await apiClient.post("/category", {
+            name: name
+        })
+
+        toast.success(`Categoria ${name} cadastrada com sucesso!`)
+        setName("")
+    }
+
+    return(
+        <>
+        <Head>
+            <title>Nova Categoria - PizzApp</title>
+        </Head>
+
+        <div>
+            <Header />
+            <main className={styles.container}>
+                <h1>Cadastrar categorias</h1>
+
+                    <form className={styles.form} onSubmit={handleRegister}>
+                    
+                    <input className={styles.input}
+                    type="text"
+                    placeholder="Digite o nome da categoria do produto"
+                    value={name}
+                    onChange={ texto => setName(texto.target.value)}
+                    />
+
+                    <button className={styles.button} type="submit">
+                        Cadastrar
+                    </button>
+                </form>
+            </main>
+        </div>
+        </>
+    )
+}
